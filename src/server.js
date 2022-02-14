e1820b490b47e9d9742edb5a330b38b8e652b90d
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-02-14 18:31:55 -0300

    update

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const server = express();
 
 server.use(cors());
 
-app.use(function (req, res, next) {
+server.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
