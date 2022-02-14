fe7248f048b7d5ed3b3b6972a24c7a718d0cbb5f
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-02-14 16:11:19 -0300

    update

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const routes = require('./routes');
 const server = express();
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://esparta-todo-api.herokuapp.com/');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
