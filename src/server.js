dfb7b43ff090db0bfb548137c5eaa447e02e33ea
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-02-14 16:04:41 -0300

    update

--- src/server.js
+++ src/server.js
@@ -10,13 +10,20 @@ const routes = require('./routes');
 
 const server = express();
 
-server.use(express.json()); 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+server.use(express.json());
 
 server.use(cors());
 
-server.use(bodyParser.urlencoded( { extended: false } ));
+server.use(bodyParser.urlencoded({ extended: false }));
 
 server.use('/api', routes);
+
 
 server.listen(process.env.PORT, () => {
     console.log(`Servidor rodando em: ${process.env.DB_HOST}:${process.env.PORT}`)
