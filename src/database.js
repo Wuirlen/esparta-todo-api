b3981fb47a8f1079a9bc9baf9d03f7b25d473fdd
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-02-14 20:26:13 -0300

    up

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
