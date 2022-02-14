49e6f4a8fceaaf6f84ef1899d6529686d2086c83
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-02-14 20:00:53 -0300

    update

--- src/database.js
+++ src/database.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+let bd_setting = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    typeCast: castField = (field, useDefaultTypeCasting) =>{
+    typeCast: castField = (field, useDefaultTypeCasting) => {
 
-        if ((field.type === "BIT") && (field.length === 1)) {
+        if ((field.type === 'BIT') && (field.length === 1)) {
             var bytes = field.buffer();
             return (bytes[0] === 1);
         }
@@ -15,11 +15,28 @@ const connection = mysql.createConnection({
         return (useDefaultTypeCasting());
 
     }
-})
+}
 
-connection.connect((error) => {
-    if (error) throw error;
-    console.log(`Conectado ao Banco de Dados: ${process.env.DB_NAME}`);
-});
+function handleDisconnect() {
+    connection = mysql.createConnection(bd_setting);
+
+    connection.connect((error) => {
+        if (error) throw error;
+        console.log(`Conectado ao Banco de Dados: ${process.env.DB_NAME}`);
+    });
+
+    connection.on('error', (error) => {
+        console.log('db error', error);
+        if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw error;
+        }
+    });
+    
+}
+
+handleDisconnect();
+
 
 module.exports = connection;
